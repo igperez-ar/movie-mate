32c0bb5a33be45f9a5d7830ec84dee4d486561f5
Ignacio Perez <ignacioperez583@gmail.com>
2025-02-03 12:22:14 +0100

    💄 fix: show header buttons only when needed

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -1,26 +1,34 @@
-import { goBack, navigate, reset } from '@core/infrastructure/navigation/root-navigation';
 import Icon from '@react-native-vector-icons/material-design-icons';
-import { TouchableOpacity } from 'react-native';
-import { GlobalRoutesEnum, MovieRoutesEnum } from 'src/shared/enums/routes';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { TouchableOpacity, View } from 'react-native';
+import { MovieRoutesEnum } from 'src/shared/enums/routes';
 import { scale } from 'src/shared/utils/sizes';
 import styled from 'styled-components/native';
 
 export const Header: React.FC = () => {
+  const nav = useNavigation();
+  const currentRoute = useRoute().name;
+
   return (
     <HeaderContainer>
-      <TouchableOpacity
-        onPress={() => goBack()}
-      >
-        {/* <Logo>MOVIEAPP</Logo> */}
-        <Icon name="arrow-left" color="white" size={scale(24)} />
-      </TouchableOpacity>
-      {/* <HeaderDivider /> */}
-      <TouchableOpacity
-        onPress={() => navigate(MovieRoutesEnum.STACK, { screen: MovieRoutesEnum.WATCHLIST })}
-      >
-        <Icon name="ticket" color="white" size={scale(24)} />
-        {/* <HeaderTitle>Watchlist</HeaderTitle> */}
-      </TouchableOpacity>
+      <View>
+        {nav.canGoBack() && (
+          <TouchableOpacity onPress={nav.goBack}>
+            <Icon name="arrow-left" color="white" size={scale(24)} />
+          </TouchableOpacity>
+        )}
+      </View>
+      <View>
+        {currentRoute !== MovieRoutesEnum.WATCHLIST && (
+          <TouchableOpacity
+            onPress={() =>
+              nav.navigate(MovieRoutesEnum.STACK, { screen: MovieRoutesEnum.WATCHLIST })
+            }
+          >
+            <Icon name="ticket" color="white" size={scale(24)} />
+          </TouchableOpacity>
+        )}
+      </View>
     </HeaderContainer>
   );
 };
@@ -29,24 +37,6 @@ const HeaderContainer = styled.View`
   justify-content: space-between;
   flex-direction: row;
   align-items: center;
-  padding: ${({ theme }) => theme.spacing['md-plus']}px ${({ theme }) => theme.spacing['lg']}px;
+  padding: ${({ theme }) => `${theme.spacing['md-plus']}px ${theme.spacing['lg']}px`};
   background-color: #1a1a1a;
-`;
-
-const Logo = styled.Text`
-  color: #e50914;
-  font-size: 24px;
-  font-weight: 700;
-`;
-
-const HeaderTitle = styled.Text`
-  color: white;
-  font-size: 18px;
-`;
-
-const HeaderDivider = styled.View`
-  height: 24px;
-  width: 1px;
-  background-color: #404040;
-  margin: 0 16px;
 `;
