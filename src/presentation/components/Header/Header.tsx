bea273d68051865fea52083ad83c2f2a5ac01193
Ignacio Perez <ignacioperez583@gmail.com>
2025-02-04 02:45:24 +0100

    💄 feat: new header design with better interactions

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -1,34 +1,42 @@
-import { Icon } from '@components/index';
+import { Icon } from '@components/Icon/Icon';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { TouchableOpacity, View } from 'react-native';
-import { MovieRoutesEnum } from 'src/shared/enums/routes';
+import { TouchableOpacity } from 'react-native';
+import { GlobalRoutesEnum, MovieRoutesEnum } from 'src/shared/enums/routes';
 import styled from 'styled-components/native';
 
 export const Header: React.FC = () => {
-  const nav = useNavigation();
+  const navigation = useNavigation();
   const currentRoute = useRoute().name;
+
+  const goToHome = () => {
+    if (currentRoute !== GlobalRoutesEnum.HOME) {
+      navigation.reset({ index: 0, routes: [{ name: GlobalRoutesEnum.HOME }] });
+    }
+  };
+
+  const goToWatchlist = () => {
+    navigation.navigate(MovieRoutesEnum.STACK, { screen: MovieRoutesEnum.WATCHLIST });
+  };
 
   return (
     <HeaderContainer>
-      <View>
-        {nav.canGoBack() && (
-          <TouchableOpacity onPress={nav.goBack}>
+      <ActionContainer align="flex-start">
+        {navigation.canGoBack() && (
+          <TouchableOpacity onPress={navigation.goBack}>
             <Icon name="arrow-left" size={24} />
           </TouchableOpacity>
         )}
-      </View>
-      <Title>MovieMate</Title>
-      <View>
+      </ActionContainer>
+      <TouchableOpacity onPress={goToHome}>
+        <Title>MovieMate</Title>
+      </TouchableOpacity>
+      <ActionContainer align="flex-end">
         {currentRoute !== MovieRoutesEnum.WATCHLIST && (
-          <TouchableOpacity
-            onPress={() =>
-              nav.navigate(MovieRoutesEnum.STACK, { screen: MovieRoutesEnum.WATCHLIST })
-            }
-          >
+          <TouchableOpacity onPress={goToWatchlist}>
             <Icon name="ticket" size={24} />
           </TouchableOpacity>
         )}
-      </View>
+      </ActionContainer>
     </HeaderContainer>
   );
 };
@@ -42,7 +50,18 @@ const HeaderContainer = styled.View`
 `;
 
 const Title = styled.Text`
+  flex: 1;
+  align-self: center;
+  text-align: center;
   color: ${({ theme }) => theme.colors.text.primary};
   font-family: Bukhari Script;
   font-size: 20px;
+`;
+
+type AnimationContainerProps = {
+  align?: string;
+};
+const ActionContainer = styled.View<AnimationContainerProps>`
+  flex: 1;
+  align-items: ${(props) => props.align};
 `;
